c7c4b391d608bb9d20dad37a285dcbc61a503aaa
kuniyoshinakamura <74222652+kuniyoshinakamura@users.noreply.github.com>
2025-01-29 08:19:33 +0900

    Update PurchaseController.java
    20250129 NakamuraKuniyoshi edit

--- src/main/java/com/example/controller/PurchaseController.java
+++ src/main/java/com/example/controller/PurchaseController.java
@@ -1,5 +1,8 @@
 package com.example.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
